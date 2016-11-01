f303ae0583347b605abf687091ecfe99429b915b
Jesse McKay and Jim Padilla <jim@>
2016-11-01 14:20:09 -0700

    Remove debugger

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,6 @@ function Customer(nameInput, initialInput) {
 //}
 
 Customer.prototype.balanceUpdate = function(depo, withd) {
-  debugger;
   return this.money += depo - withd
 }
 
@@ -20,7 +19,6 @@ var Customers = [];
 $(document).ready(function() {
 
   $("form#registration").submit(function(event) {
-    debugger;
     event.preventDefault();
     var nameInput = $("input#name").val();
     var initialInput = parseInt($("input#number").val());
@@ -32,7 +30,6 @@ $(document).ready(function() {
 
   $("form#atm").submit(function(event) {
     event.preventDefault();
-    debugger;
     var depositInput = parseInt($("input#deposit").val());
     var withdrawalInput = parseInt($("input#withdrawal").val());
     var totalChange = Customers[0].balanceUpdate(depositInput, withdrawalInput);
